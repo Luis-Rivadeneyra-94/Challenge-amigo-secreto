e511052b784610f6df93fd78ecd2e95aecd7c9ef
Luis Rivadeneyra <nilo_822@hotmail.com>
2025-08-03 21:23:39 -0500

    Función agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // Creación de array
 Let amigo = [];
+function agregarAmigo(){
+    const nombreIngresado=document.getElementById("amigo").value
+    if(nombreIngresado==""){
+        alert("Por favor, inserte un nombre.);
+    }
+    else{
+    amigo.push(nombreIngresado);
+    document.getElementById("amigo").value="";    
+    }
+}
